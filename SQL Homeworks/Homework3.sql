d7421eeda081d9c47dcb790ca8e6fe16151f3a32
Mücahit Erkan Oygun <56897548+erkanoygun@users.noreply.github.com>
2024-02-29 21:48:27 +0300

    Add Sql files.

--- SQL Homeworks/Homework3.sql
+++ SQL Homeworks/Homework3.sql
@@ -1,11 +1,11 @@
-SELECT country FROM country WHERE country LIKE 'A%a'
+SELECT country FROM country WHERE country LIKE 'A%a';
 
 SELECT first_name, last_name FROM actor
-WHERE first_name IN ('Penelope', 'Nick','Ed')
+WHERE first_name IN ('Penelope', 'Nick','Ed');
 
-SELECT country FROM country WHERE country LIKE '_____%n'
+SELECT country FROM country WHERE country LIKE '_____%n';
 
-SELECT title FROM film WHERE title ILIKE '%T%T%T%T%'
+SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
 
 SELECT title FROM film
-WHERE title ILIKE 'C%' AND length > 90 AND rental_rate = 2.99
+WHERE title ILIKE 'C%' AND length > 90 AND rental_rate = 2.99;
