70ce5707296dffbd14d00341b46833778dcb80e4
Mücahit Erkan Oygun <56897548+erkanoygun@users.noreply.github.com>
2024-03-01 19:29:51 +0300

    Add Sql files.

---
+++ SQL Homeworks/Homework8.sql
@@ -0,0 +1,26 @@
+--test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+
+--CREATE TABLE employee
+--(
+--	id INTEGER, name VARCHAR(50), birthday DATE, email VARCHAR(100)
+--);
+
+
+--Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+
+--insert into employee (id, name, birthday, email) values (2, 'Barbie', '1969-11-03', 'bjoul1@netvibes.com');
+--insert into employee (id, name, birthday, email) values (3, 'Jory', '2003-01-30', 'jfarr2@dailymotion.com');
+
+--Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+
+--UPDATE employee SET name = 'Ahmet'
+--WHERE id = 1;
+
+
+--Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+--DELETE FROM employee WHERE id = 5;
+
+
+
+
+
